065ac864a1cc7604d615ad35e0516f05fb2586a5
phantiensy <phantiensy195@gmail.com>
2018-09-20 09:56:39 +0700

    ProductController edit

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -1,10 +1,14 @@
 package com.codegym.controller;
 
 import com.codegym.model.Product;
+import com.codegym.model.User;
 import com.codegym.service.product.ProductService;
+import com.codegym.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +22,21 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private UserService userService;
+
+    private String getPrincipal() {
+        String email = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            email = ((UserDetails) principal).getUsername();
+        } else {
+            email = principal.toString();
+        }
+        return email;
+    }
+
     @GetMapping("/products")
     public String listProduct(Model model, Pageable pageable) {
         Page<Product> products = productService.findAll(pageable);
@@ -27,6 +46,8 @@ public class ProductController {
 
     @GetMapping("/products/create")
     public String showFormCreate(Model model) {
+        User user = userService.findUserByEmail(getPrincipal());
+        model.addAttribute("user", user);
         model.addAttribute("product", new Product());
         return "/product/create";
     }
@@ -34,10 +55,12 @@ public class ProductController {
     @PostMapping("/products/create")
     public String createProduct(@ModelAttribute("product") Product product, Model model) {
         productService.save(product);
+        User user = userService.findUserByEmail(getPrincipal());
 
+        model.addAttribute("user", user);
         model.addAttribute("product", product);
         model.addAttribute("message", "Product created successfully");
-        return "/product/create";
+        return "/image/upload";
     }
 
     @GetMapping("/products/edit/{id}")
@@ -82,5 +105,15 @@ public class ProductController {
 
         model.addAttribute("product", product);
         return "/product/detail";
+    }
+
+    @GetMapping("/products/upload/{id}")
+    public String fileUpload(@PathVariable("id") Long id, Model model) {
+        Product product = productService.findById(id);
+        User user = userService.findUserByEmail(getPrincipal());
+
+        model.addAttribute("user", user);
+        model.addAttribute("product", product);
+        return "/image/upload";
     }
 }
