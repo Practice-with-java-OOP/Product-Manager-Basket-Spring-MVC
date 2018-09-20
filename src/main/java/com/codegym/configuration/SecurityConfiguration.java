bce7bee374dda0b2aebe893c96dda6b114032d62
phantiensy <phantiensy195@gmail.com>
2018-09-20 11:45:44 +0700

    SecurityConfiguration edit

--- src/main/java/com/codegym/configuration/SecurityConfiguration.java
+++ src/main/java/com/codegym/configuration/SecurityConfiguration.java
@@ -22,7 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/home", "/resource/**", "/users/create").permitAll()
+        http.authorizeRequests().antMatchers("/", "/home", "/resource/**", "/users/create", "/products/detail/**").permitAll()
                 .antMatchers("/user/**").access("hasRole('USER')")
                 .antMatchers("/admin/**").access("hasRole('ADMIN')")
                 .anyRequest().authenticated()
